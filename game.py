7d240269efeba8d3cec5cc57b904595f0be36803
ShintaroSugawara <sugawgra.shintaro@gmail.com>
2026-07-30 15:48:01 +0900

    ver.1.1

--- game.py
+++ game.py
@@ -1,21 +1,28 @@
 import random
 
-Win = True
+win = True
 
 win_count = 0
 
 
-while Win:
+while win:
 
    cpu_card = random.randint(1,13)
-
    print("CPUのカードは:",cpu_card,"です。")
 
    player_card = random.randint(1,13)
 
    print("1:High or 2:Low or 3:Break")
-
    high_low = int(input())
+   
+   while high_low != 1 and high_low != 2 and high_low != 3:
+      print("1~3の数字を入力してください。")
+      high_low = int(input())
+
+   if high_low == 3:
+      break
+
+   print("あなたのカードは:",player_card,"でした。")
 
    if high_low == 1:
       if player_card > cpu_card:
@@ -23,23 +30,21 @@ while Win:
          win_count += 1
       elif player_card < cpu_card:
          print("Lose")
-         Win = False
+         win = False
       else:
-         print("Drow")
+         print("Draw")
 
-   if high_low == 2:
+   elif high_low == 2:
       if player_card < cpu_card:
          print("Win!!")
          win_count += 1
       elif player_card > cpu_card:
          print("Lose")
-         Win = False
+         win = False
       else:
-         print("Drow")
-
-   if high_low == 3:
-      break
-
-   print("あなたのカードは:",player_card,"でした。")
+         print("Draw")
 
    print("勝利回数:",win_count)
+
+print("ゲーム終了")
+print("最終勝利回数:", win_count)
